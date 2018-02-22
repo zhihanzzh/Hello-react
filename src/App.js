9f5c267626e372c87a7be1779963002f9abfb791
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-02-21 20:40:50 -0500

    2nd component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
@@ -8,7 +9,17 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-3">
-          <h1>Hello React</h1>
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <h1>Hello React</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <Home />
           </div>
         </div>
       </div>
